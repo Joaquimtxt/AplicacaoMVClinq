6e76559b3dd2d79878810823a84bbc401e533ebe
Joaquimtxt <jocaq124@gmail.com>
2024-08-27 14:54:48 -0300

    Adicionar arquivos de projeto.

---
+++ AplicacaoMVClinq/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AplicacaoMVClinq.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
