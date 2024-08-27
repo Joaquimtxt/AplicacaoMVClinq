86845bf16f47905132a53aac6ec3966cab926737
Joaquimtxt <jocaq124@gmail.com>
2024-08-27 16:34:57 -0300

    Finalizando a lista de clientes

---
+++ AplicacaoMVClinq/Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using AplicacaoMVClinq.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicacaoMVClinq.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetClientes()
+        {
+            Cliente c1 = new Cliente(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c2 = new Cliente(2, "Bob", "bob@example.com", "2345-6789", new DateOnly(2001, 10, 06));
+            Cliente c3 = new Cliente(3, "Charlie", "charlie@example.com", "3456-7890", new DateOnly(1999, 12, 06));
+            Cliente c4 = new Cliente(4, "David", "david@example.com", "4567-8901", new DateOnly(2000, 12, 01));
+            Cliente c5 = new Cliente(5, "Eve", "eve@example.com", "5678-9012", new DateOnly(2000, 12, 06));
+            List<Cliente> listaClientes = new List<Cliente>();
+            //Adicinar os Objetos Clientes na lista Clientes
+            listaClientes.Add(c1);
+            listaClientes.Add(c2);
+            listaClientes.Add(c3);
+            listaClientes.Add(c4);
+            listaClientes.Add(c5);
+
+            return View(listaClientes);
+        }
+    }
+}
---
+++ AplicacaoMVClinq/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿namespace AplicacaoMVClinq.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateOnly DataNasc { get; set; }
+        public Cliente(int id, string Nome, string Email, string Telefone, DateOnly DataNasc)
+        {
+            this.ClienteId = id;
+            this.Nome = Nome;
+            this.Email = Email;
+            this.Telefone = Telefone;
+            this.DataNasc = DataNasc;
+        }
+    }
+
+}
